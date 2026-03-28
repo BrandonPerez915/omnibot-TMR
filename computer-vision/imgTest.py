bce91208a11567ebe2427bde16cc4eca1c48d1bb
Ivan <diego.sigala@iteso.mx>
2026-03-27 22:30:49 -0400

    modified img test to use the optimized model

--- computer-vision/imgTest.py
+++ computer-vision/imgTest.py
@@ -1,56 +1,56 @@
 from ultralytics import YOLO
 import cv2 as cv
-
+import matplotlib.pyplot as plt
 from utils.results import filterResults
 
+MODEL_PATH = "./model/model.engine" 
+IMG_PATH = "../dataset/img2.jpg"
 
-MODEL_PATH = "./model/model.pt"
-IMG_PATH = "../data/test/img5.png"
+model = YOLO(MODEL_PATH, task='detect')
 
-model = YOLO(MODEL_PATH)
-rawResults = model.predict(IMG_PATH, conf=0.5)[0]
-
+rawResults = model.predict(IMG_PATH, conf=0.5, device=0)[0]
 img = cv.imread(IMG_PATH)
 data = filterResults(rawResults, img)
 
+l_vals, a_vals, b_vals, colors = [], [], [], []
+
 for bean in data["beans"]:
-    color = None
+    l_vals.append(bean.l)
+    a_vals.append(bean.a)
+    b_vals.append(bean.b)
+
     if bean.colorName == "Sobremaduro":
-        color = (0, 0, 0)
+        draw_color = (0, 0, 0)      # BGR para OpenCV
+        plot_color = (0, 0, 0)      # RGB para Plot
     elif bean.colorName == "Maduro":
-        color = (0, 0, 255)
+        draw_color = (255, 0, 0)    # BGR (Azul)
+        plot_color = (0, 0, 1)      # RGB (Azul)
     else:
-        color = (0, 255, 0)
+        draw_color = (0, 255, 0)    # BGR (Verde)
+        plot_color = (0, 1, 0)      # RGB (Verde)
 
-    cv.rectangle(img, (bean.x1, bean.y1), (bean.x2, bean.y2), color, 2)
-    label_text = bean.colorName
-    cv.putText(
-        img,
-        label_text,
-        (bean.x1, bean.y1 - 10),
-        cv.FONT_HERSHEY_SIMPLEX,
-        0.5,
-        color,
-        2,
-    )
+    colors.append(plot_color)
+    
+    cv.rectangle(img, (bean.x1, bean.y1), (bean.x2, bean.y2), draw_color, 2)
+    cv.putText(img, bean.colorName, (bean.x1, bean.y1 - 10), 
+               cv.FONT_HERSHEY_SIMPLEX, 0.5, draw_color, 2)
 
-for tree in data["trees"]:
-    cv.rectangle(img, (tree.x1, tree.y1), (tree.x2, tree.y2), (255, 0, 0), 2)
+fig = plt.figure(figsize=(15, 7))
 
-# # Configurar la gráfica 3D
-# fig = plt.figure(figsize=(10, 7))
-# ax = fig.add_subplot(111, projection="3d")
+ax_img = fig.add_subplot(1, 2, 1)
+img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+ax_img.imshow(img_rgb)
+ax_img.set_title("Detecciones YOLO")
+ax_img.axis("off") 
 
-# l_vals, a_vals, b_vals, colors = [], [], [], []
+ax_3d = fig.add_subplot(1, 2, 2, projection="3d")
 
-# if len(data["beans"]) > 0:
-#     ax.scatter(a_vals, b_vals, l_vals, c=colors, s=60, edgecolors="k")
-#     ax.set_xlabel("Canal a (Verde <-> Rojo)")
-#     ax.set_ylabel("Canal b (Azul <-> Amarillo)")
-#     ax.set_zlabel("Canal L (Luminosidad)")
-#     ax.set_title("Distribución de Granos en Espacio LAB")
-#     plt.show()
+if len(data["beans"]) > 0:
+    ax_3d.scatter(a_vals, b_vals, l_vals, c=colors, s=60, edgecolors="k")
+    ax_3d.set_xlabel("Canal a")
+    ax_3d.set_ylabel("Canal b")
+    ax_3d.set_zlabel("Luminosidad (L)")
+    ax_3d.set_title("Espacio de Color LAB")
 
-cv.imshow("Result", img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+plt.tight_layout()
+plt.show()
